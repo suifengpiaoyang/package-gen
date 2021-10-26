0d138fbe262c45787fb51601ffd58bcd33bf3979
Zhang <504947985@qq.com>
2021-10-26 16:13:45 +0800

    增加包的初始信息

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ import setuptools
 setuptools.setup(
     name="packagegenerator",
     version="0.0.2",
-    author="",
+    author="Zhang",
     author_email="",
+    url = 'https://github.com/suifengpiaoyang/packagegenerator',
     description="generate python package framework",
     packages=setuptools.find_packages(),
     include_package_data=True,
